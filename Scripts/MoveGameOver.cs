c91a49dba8b9973b0d41a69a58b1f7b8a94db078
Murasho7164 <87461873+Murasho7164@users.noreply.github.com>
2022-01-19 02:42:08 +0900

    1

---
+++ Scripts/MoveGameOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveGameOver : MonoBehaviour
+{
+    private GameObject gameManager;
+    // Start is called before the first frame update
+    void Start(){
+        gameManager =GameObject.Find("GameManager");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("GG");
+        StartCoroutine("MoveGameOverCoroutine");
+    }
+
+    IEnumerator MoveGameOverCoroutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+        gameManager.GetComponent<GameManager>().GameOver();
+        yield return null;
+    }
+}
